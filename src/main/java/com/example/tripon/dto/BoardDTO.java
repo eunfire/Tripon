45112d007a0dde165eb8bf19e75b91bdee72518e
CHAYOUBIN <youbin5412@naver.com>
2023-09-19 14:21:40 +0900

    feat: 커뮤니티 게시판 조회

---
+++ src/main/java/com/example/tripon/dto/BoardDTO.java
@@ -0,0 +1,20 @@
+package com.example.tripon.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.ibatis.type.Alias;
+
+import java.time.LocalDateTime;
+
+@Alias("boardDTO")
+@Getter
+@Setter
+public class BoardDTO {
+    private int boardId;
+    private String memId;
+    private int cateId;
+    private String title;
+    private String content;
+    private LocalDateTime createdTime;
+    private int views;
+}
