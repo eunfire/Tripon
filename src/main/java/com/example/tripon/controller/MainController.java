1330255c9f58f8307704ba12cc1258122d28b972
CHAYOUBIN <youbin5412@naver.com>
2023-10-26 14:15:20 +0900

    메인페이지 최신글 조회, 페이징처리 메인페이지 검색결과 조회(제목으로 구현) ## 추후 CSS수정 필요(검색칸, 최신글 글씨, 최신글 제목칸)

--- src/main/java/com/example/tripon/controller/MainController.java
+++ src/main/java/com/example/tripon/controller/MainController.java
@@ -1,15 +1,97 @@
 package com.example.tripon.controller;
 
+import com.example.tripon.dto.BoardDTO;
+import com.example.tripon.service.MainBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @RequiredArgsConstructor
+@RequestMapping("/Main")
 @Controller
 public class MainController {
+    private final MainBoardService mainBoardService;
+    @GetMapping("/board")
+    public String getAllPosts(@RequestParam(defaultValue = "1") int page, Model model) {
+        int pageSize = 8; // 페이지당 게시글 수
+        // 해당 페이지의 시작 인덱스 계산
+        int startIndex = (page - 1) * pageSize;
 
-    @GetMapping("/main")
-    public String mainpage() {
-        return "/main";
+        // 전체 게시글 목록을 조회
+        List<BoardDTO> allPosts = mainBoardService.getAllPosts(startIndex, pageSize);
+
+        model.addAttribute("posts", allPosts);
+
+        // 현재 페이지 번호와 총 페이지 수 계산하여 모델에 추가
+        int totalPosts = mainBoardService.getAllPostCount();
+        int totalPages = (int) Math.ceil((double) totalPosts / pageSize);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", totalPages);
+
+        return "Main";
+    }
+
+    @GetMapping("/search-all")
+    public String getAllPosts(@RequestParam(defaultValue = "1") int page,
+                              @RequestParam(name = "search", required = false) String search,
+                              Model model) {
+        int pageSize = 10; // 페이지당 게시글 수
+        int startIndex = (page - 1) * pageSize;
+
+        List<BoardDTO> allPosts;
+
+        if (search != null && !search.isEmpty()) {
+            // 검색어를 이용하여 게시글을 검색
+            allPosts = mainBoardService.searchPostsByTitle(search, startIndex, pageSize);
+        } else {
+            // 전체 게시글 목록을 조회
+            allPosts = mainBoardService.getAllPosts(startIndex, pageSize);
+        }
+
+        model.addAttribute("posts", allPosts);
+
+        int totalPosts = mainBoardService.getAllPostCount();
+        int totalPages = (int) Math.ceil((double) totalPosts / pageSize);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", totalPages);
+
+        // 페이징 숫자 범위 계산
+        int pagesToShow = 10; // 10 페이지까지 표시
+        int startPage = Math.max(1, page - (pagesToShow / 2));
+        int endPage = Math.min(totalPages, startPage + pagesToShow - 1);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "search-all";
+    }
+
+    @GetMapping("/newest")
+    public String getAllPosts2(@RequestParam(defaultValue = "1") int page, Model model) {
+        int pageSize = 10; // 페이지당 게시글 수
+        int startIndex = (page - 1) * pageSize;
+
+        List<BoardDTO> allPosts = mainBoardService.getAllPosts(startIndex, pageSize);
+        model.addAttribute("posts", allPosts);
+
+        int totalPosts = mainBoardService.getAllPostCount();
+        int totalPages = (int) Math.ceil((double) totalPosts / pageSize);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", totalPages);
+
+        // 페이징 숫자 범위 계산
+        int pagesToShow = 10; // 10 페이지까지 표시
+        int startPage = Math.max(1, page - (pagesToShow / 2));
+        int endPage = Math.min(totalPages, startPage + pagesToShow - 1);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "/newest-board";
     }
 }
--- src/main/java/com/example/tripon/repository/MainBoardRepository.java
+++ src/main/java/com/example/tripon/repository/MainBoardRepository.java
@@ -1,4 +1,37 @@
 package com.example.tripon.repository;
 
+import com.example.tripon.dto.BoardDTO;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+@RequiredArgsConstructor
 public class MainBoardRepository {
+    private final SqlSessionTemplate sql;
+
+    // 전체 게시글 목록 조회
+    public List<BoardDTO> getAllPosts(int startIndex, int pageSize) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("startIndex", startIndex);
+        params.put("pageSize", pageSize);
+        return sql.selectList("main.getAllPosts", params);
+    }
+
+    // 전체 게시글 수 조회
+    public int getAllPostCount() {
+        return sql.selectOne("main.getAllPostCount");
+    }
+
+    public List<BoardDTO> searchPostsByTitle(String title, int startIndex, int pageSize) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("title", title);
+        params.put("startIndex", startIndex);
+        params.put("pageSize", pageSize);
+        return sql.selectList("main.searchPostsByTitle", params);
+    }
 }
